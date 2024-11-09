cc588146cd8cd09a5f105a81945e71b11e7f014b
Daniil <astafiev.den@gmail.com>
2024-11-09 18:43:06 +0300

    added liquibase changesets

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset dan:1
+CREATE TABLE IF NOT EXISTS users
+(
+    id BIGSERIAL PRIMARY KEY ,
+    username VARCHAR(64) NOT NULL UNIQUE ,
+    password_hash VARCHAR(256) NOT NULL ,
+    created_at TIMESTAMP NOT NULL ,
+    last_modified_at TIMESTAMP
+);
+
+--changeset dan:2
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id BIGSERIAL PRIMARY KEY ,
+    title VARCHAR(32) NOT NULL ,
+    description VARCHAR(128) ,
+    completed BOOLEAN NOT NULL ,
+    created_at TIMESTAMP NOT NULL ,
+    last_modified_at TIMESTAMP ,
+    user_id INT REFERENCES users (id) ON DELETE CASCADE
+);

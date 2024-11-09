6c1327ed8af2b000303b9d7558f7c03f588c1916
Daniil <astafiev.den@gmail.com>
2024-11-09 22:54:01 +0300

    updated changelogs

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -3,20 +3,9 @@
 --changeset dan:1
 CREATE TABLE IF NOT EXISTS users
 (
-    id BIGSERIAL PRIMARY KEY ,
+    id SERIAL PRIMARY KEY ,
     username VARCHAR(64) NOT NULL UNIQUE ,
     password_hash VARCHAR(256) NOT NULL ,
     created_at TIMESTAMP NOT NULL ,
     last_modified_at TIMESTAMP
-);
-
---changeset dan:2
-CREATE TABLE IF NOT EXISTS tasks
-(
-    id BIGSERIAL PRIMARY KEY ,
-    title VARCHAR(32) NOT NULL ,
-    completed BOOLEAN NOT NULL ,
-    created_at TIMESTAMP NOT NULL ,
-    last_modified_at TIMESTAMP ,
-    user_id INT REFERENCES users (id) ON DELETE CASCADE
-);
+)
---
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset dan:1
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id BIGSERIAL PRIMARY KEY ,
+    title VARCHAR(32) NOT NULL ,
+    completed BOOLEAN NOT NULL ,
+    created_at TIMESTAMP NOT NULL ,
+    last_modified_at TIMESTAMP ,
+    user_id INT REFERENCES users (id) ON DELETE CASCADE
+)

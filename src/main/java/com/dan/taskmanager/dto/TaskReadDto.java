71d3d47eeb0a3c9bf180fe7191d502c1a65f9ba4
Daniil <astafiev.den@gmail.com>
2024-11-11 22:21:14 +0300

    updated dto

--- src/main/java/com/dan/taskmanager/dto/TaskReadDto.java
+++ src/main/java/com/dan/taskmanager/dto/TaskReadDto.java
@@ -2,10 +2,13 @@ package com.dan.taskmanager.dto;
 
 import lombok.Value;
 
+import java.time.Instant;
+
 @Value
 public class TaskReadDto {
     Long id;
     String title;
     Boolean completed;
+    Instant createdAt;
     UserReadDto user;
 }

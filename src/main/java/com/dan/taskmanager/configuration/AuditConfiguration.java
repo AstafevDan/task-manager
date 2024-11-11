90b274ccc997d3812d44df32bc22bb14d9d8ce9f
Daniil <astafiev.den@gmail.com>
2024-11-11 22:16:20 +0300

    added AuditConfiguration

---
+++ src/main/java/com/dan/taskmanager/configuration/AuditConfiguration.java
@@ -0,0 +1,9 @@
+package com.dan.taskmanager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditConfiguration {
+}

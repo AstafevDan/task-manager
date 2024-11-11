7689dcbbdca64e7f5c1cfa53c20fc4b00c217716
Daniil <astafiev.den@gmail.com>
2024-11-11 22:22:13 +0300

    updated TaskService

--- src/main/java/com/dan/taskmanager/service/TaskService.java
+++ src/main/java/com/dan/taskmanager/service/TaskService.java
@@ -1,6 +1,8 @@
 package com.dan.taskmanager.service;
 
+import com.dan.taskmanager.dto.TaskCreateEditDto;
 import com.dan.taskmanager.dto.TaskReadDto;
+import com.dan.taskmanager.mapper.TaskCreateEditMapper;
 import com.dan.taskmanager.mapper.TaskReadMapper;
 import com.dan.taskmanager.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
     private final TaskReadMapper taskReadMapper;
+    private final TaskCreateEditMapper taskCreateEditMapper;
 
     public List<TaskReadDto> findAll() {
         return taskRepository.findAll()
@@ -27,5 +30,33 @@ public class TaskService {
 
     public Optional<TaskReadDto> findById(Long id) {
         return taskRepository.findById(id).map(taskReadMapper::map);
+    }
+
+    @Transactional
+    public TaskReadDto create(TaskCreateEditDto dto) {
+        return Optional.of(dto)
+                .map(taskCreateEditMapper::map)
+                .map(taskRepository::save)
+                .map(taskReadMapper::map)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public Optional<TaskReadDto> update(Long id, TaskCreateEditDto dto) {
+        return taskRepository.findById(id)
+                .map(entity -> taskCreateEditMapper.map(dto, entity))
+                .map(taskRepository::saveAndFlush)
+                .map(taskReadMapper::map);
+    }
+
+    @Transactional
+    public boolean delete(Long id) {
+        return taskRepository.findById(id)
+                .map(entity -> {
+                    taskRepository.delete(entity);
+                    taskRepository.flush();
+                    return true;
+                })
+                .orElse(false);
     }
 }

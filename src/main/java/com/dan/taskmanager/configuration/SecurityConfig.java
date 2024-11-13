4d4e3575ae25d57bb2ff7b44c3a0efef0771b1ed
Daniil <astafiev.den@gmail.com>
2024-11-14 00:15:56 +0300

    added SecurityConfig

---
+++ src/main/java/com/dan/taskmanager/configuration/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.dan.taskmanager.configuration;
+
+import com.dan.taskmanager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(httpForm -> {
+                    httpForm.loginPage("/login").permitAll();
+                    httpForm.defaultSuccessUrl("/tasks");
+                    httpForm.failureUrl("/login?error=true");
+                })
+
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/registration", "/css/**", "/js/**").permitAll();
+                    registry.anyRequest().authenticated();
+                })
+
+                .build();
+    }
+}
